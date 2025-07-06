0634a6f402cd618a3c7dc0e300a9c09b7d52c104
NBlad3 <NBFriend123@outlook.com>
2025-07-06 09:13:03 +0200

    completed blog

--- my_students/students/models.py
+++ my_students/students/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+  name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  gender = models.CharField(max_length=255)
+  birthdate = models.CharField(max_length=255)
